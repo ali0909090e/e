1a61f4dfbf986876ac2fc359d5901922760fe02b
ali0909090e <88100612+ali0909090e@users.noreply.github.com>
2021-09-08 17:30:47 +0530

    Add files via upload

---
+++ RHDTzfmhxbmcn,gmyjrtjrfhgxxgtheanfgbdmhtyhretnfgxmdgmyjtharmhsgshmyjtarmfhz hmrjtangfzn.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+
+function preload(){
+
+move = loadAnimation("add image urls here");
+drink = loadAnimation("add image urls here");
+gym = loadAnimation("add image urls here");
+brush = loadAnimation("add image urls here");
+bath = loadAnimation("add image urls here");
+mave = loadAnimatoin("add image urls here");
+}
+setbackground(40);
+
+
+
+}
+
+function draw() {
+  background(220);
+}
